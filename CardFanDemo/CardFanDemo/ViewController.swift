00f7d58c1a0160f98a1d0684fe9049703ff23b25
German Azcona <german.azcona@airtime.com>
2022-05-04 13:50:47 -0300

    Added gif to documentation

--- CardFanDemo/CardFanDemo/ViewController.swift
+++ CardFanDemo/CardFanDemo/ViewController.swift
@@ -16,26 +16,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
+        // cardFan.subviews.first?.backgroundColor = .red.withAlphaComponent(0.2)
+        cardFan.backgroundColor = .clear
+
         cardFan.cardSize = .init(width: 255, height: 340)
         cardFan.minCardScale = 0.5
         cardFan.maxXTranslate = 80
         cardFan.maxRotation = -.pi/12
         cardFan.numberOfVisibleSideCards = 5
-        cardFan.subviews.first?.backgroundColor = .red.withAlphaComponent(0.2)
-        cardFan.appliesAlignmentCorrection = true
+        cardFan.appliesAlignmentCorrection = false
         cardFan.cardViews = [
-            cardView(backgroundColor: .systemPink),
+            // cardView(backgroundColor: .systemPink),
             cardView(backgroundColor: .systemRed),
             cardView(backgroundColor: .systemOrange),
-            cardView(backgroundColor: .systemBrown),
+            // cardView(backgroundColor: .systemBrown),
             cardView(backgroundColor: .systemYellow),
             cardView(backgroundColor: .systemGreen),
-            cardView(backgroundColor: .systemMint),
-            cardView(backgroundColor: .systemCyan),
+            // cardView(backgroundColor: .systemMint),
+            // cardView(backgroundColor: .systemCyan),
             cardView(backgroundColor: .systemTeal),
             cardView(backgroundColor: .systemBlue),
             cardView(backgroundColor: .systemPurple),
-            cardView(backgroundColor: .systemGray),
+            // cardView(backgroundColor: .systemGray),
         ]
     }
 
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 A stack of views that looks like a fan of cards. It lets the user scroll left and right through the views like Apple's
 messages app.
+
+![](https://github.com/igerm/CardFan/blob/main/doc/example.gif)
